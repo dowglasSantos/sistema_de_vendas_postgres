06de5c47f59c41cb668ef3a0b124c81e3b65c6df
DOUGLAS SANTOS <78913062+dowglasSantos@users.noreply.github.com>
2025-12-01 18:31:01 -0300

    Update KeyLoginDataBase.java

--- src/database/KeyLoginDataBase.java
+++ src/database/KeyLoginDataBase.java
@@ -7,10 +7,10 @@ public class KeyLoginDataBase {
     }
 
     public String getUser() {
-        return "postgres";
+        return " ";
     }
 
     public String getPassword() {
-        return "admin";
+        return " ";
     }
 }
